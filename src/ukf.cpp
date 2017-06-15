fcab5cde8eb8c86c093115326d1d8a5480667e6e
Yang Liu <yliuosu@gmail.com>
2017-06-14 22:44:26 -0500

    feat: Complete the initialization part of ukf.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -51,6 +51,34 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  
+  // initially set to false, set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+
+  // time when the state is true, in us
+  time_us_ = 0.0;
+
+  // state dimension
+  n_x_ = 5;
+
+  // Augmented state dimension
+  n_aug_ = 7;
+
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_x_;
+
+  // predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
+  //create vector for weights
+  weights_ = VectorXd(2 * n_aug_ + 1);
+
+  // the current NIS for radar
+  NIS_radar_ = 0.0;
+
+  // the current NIS for laser
+  NIS_laser_ = 0.0;
+  
 }
 
 UKF::~UKF() {}
