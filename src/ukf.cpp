cf18947c7b1a9ffba0421c1a8b9e990eea57b7b8
Yang Liu <yliuosu@gmail.com>
2017-06-17 22:23:12 -0500

    feat: Implement the updateLidar function of the ukf.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -300,14 +300,84 @@ void UKF::Prediction(double delta_t) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-  TODO:
+	/**
+	TODO:
+	Complete this function! Use lidar data to update the belief about the object's
+	position. Modify the state vector, x_, and covariance, P_.
+	need to calculate the lidar NIS.
+	*/
+  
+	//extract measurement as VectorXd
+	VectorXd z = meas_package.raw_measurements_;
 
-  Complete this function! Use lidar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
+	//set measurement dimension, lidar can measure p_x and p_y
+	int n_z = 2;
 
-  You'll also need to calculate the lidar NIS.
-  */
+	 //create matrix for sigma points in measurement space
+	MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug_ + 1);
+
+	 //transform sigma points into measurement space
+	for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+
+		// extract values for better readibility
+		double p_x = Xsig_pred_(0, i);
+		double p_y = Xsig_pred_(1, i);
+
+		// measurement model
+		Zsig(0, i) = p_x;
+		Zsig(1, i) = p_y;
+	}
+
+	//mean predicted measurement
+	VectorXd z_pred = VectorXd(n_z);
+	z_pred.fill(0.0);
+	for (int i = 0; i < 2 * n_aug_ + 1; i++) {
+		z_pred = z_pred + weights_(i) * Zsig.col(i);
+	}
+
+	//measurement covariance matrix S
+	MatrixXd S = MatrixXd(n_z, n_z);
+	S.fill(0.0);
+	for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+		//residual
+		VectorXd z_diff = Zsig.col(i) - z_pred;
+		S = S + weights_(i) * z_diff * z_diff.transpose();
+	}
+
+	//add measurement noise covariance matrix
+	MatrixXd R = MatrixXd(n_z, n_z);
+	R << std_laspx_*std_laspx_, 0,
+	     0, std_laspy_*std_laspy_;
+	S = S + R;
+
+	//create matrix for cross correlation Tc
+	MatrixXd Tc = MatrixXd(n_x_, n_z);
+
+	/*****************************************************************************
+	*  UKF Update for Lidar
+	****************************************************************************/
+	//calculate cross correlation matrix
+	Tc.fill(0.0);
+	for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+		//residual
+		VectorXd z_diff = Zsig.col(i) - z_pred;
+		// state difference
+		VectorXd x_diff = Xsig_pred_.col(i) - x_;
+		Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
+	}
+
+	//Kalman gain K;
+	MatrixXd K = Tc * S.inverse();
+
+	//residual
+	VectorXd z_diff = z - z_pred;
+
+	//calculate NIS
+	NIS_laser_ = z_diff.transpose() * S.inverse() * z_diff;
+
+	//update state mean and covariance matrix
+	x_ = x_ + K * z_diff;
+	P_ = P_ - K*S*K.transpose();
 }
 
 /**
